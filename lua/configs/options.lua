d9fcf1e81fdf007614bbf225a58ecff4d2ca4ce9
codemaestro <folidominic97@gmail.com>
2025-06-20 09:36:00 +0100

    Some more updates

--- lua/configs/options.lua
+++ lua/configs/options.lua
@@ -1,8 +1,20 @@
-vim.g.mapleader = " "
-vim.g.maplocalleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.termguicolors = true      -- True color support
+vim.opt.scrolloff = 8             -- Keep 8 lines above/below cursor
+vim.opt.backup = false
+vim.opt.smartindent = true        -- Smart indentation
+vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.scrolloff = 4
 
+vim.highlight.priorities.semantic_tokens =  95
+
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.keymap.set("n", "<leader>w", ":w<CR>",  {desc = "Save File"})
+vim.keymap.set("n", "<leader>q", ":q<CR>", {desc = "Quit File"})
+vim.keymap.set('n', '<leader>e', ':Neotree toggle<CR>', { desc = "Toggle Neo-tree" })
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
@@ -14,3 +26,4 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.hl.on_yank()
   end,
 })
+
